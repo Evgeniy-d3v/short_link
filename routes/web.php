e904e694836fdb88d5030cd578365f62e5789de4
evgenij <evgenijdudarenko2@gmail.com>
2026-03-20 13:34:30 +0300

    refactor route

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,14 @@
 <?php
-
 use App\Presentation\AuthController;
 use App\Presentation\LinksController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/auth', [AuthController::class, 'index'])->name('auth.index');
+Route::post('/auth/login', [AuthController::class, 'login'])->name('auth.login');
+
 Route::middleware('auth.user')->group(function () {
-    Route::get('/', [LinksController::class, 'index']);
-    Route::post('/shorten', [LinksController::class, 'store']);
+    Route::get('/', [LinksController::class, 'index'])->name('links.index');
+    Route::post('/shorten', [LinksController::class, 'store'])->name('links.store');
 });
 
-Route::get('/auth', [AuthController::class, 'index']);
-Route::post('/auth/login', [AuthController::class, 'login']);
-Route::get('/s/{code}', [LinksController::class, 'redirect']);
+Route::get('/{code}', [LinksController::class, 'redirect'])->name('links.redirect');
